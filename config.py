a291e54063f9d92cbc6b61a2da20cd446a0d8c7a
Zeay999 <125983855+Zeay999@users.noreply.github.com>
2023-05-07 21:22:55 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "MTEwNDM5NjY3NzU2MzAyNzU3Nw.GUGpqs.j9ddi1MWcLtRYK9gznghQbUtZNUloZ6YdMLEn8"
+ID_POST = 1104794637077577869
+USER_ROLES_LIST = ()
+MAX_ROLES = 1
+
+ROLES_LIST = {
+    "👍": 1104793378857701417,  # гриня
+    "🤝": 1104793421308231781,  # ботев
+    "👎": 1104793451951825028,  # химматуло
+
+}
